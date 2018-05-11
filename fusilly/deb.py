8e3e17abd9632d976125b434c1cd6cde327ce78c
Shaw Vrana <shaw@vranix.com>
2018-05-11 13:12:06 -0700

    Pass input files from a temporary file
    ..command line was too long for larger projects
    default .deb to bzip2 compression if not specified in target

--- fusilly/deb.py
+++ fusilly/deb.py
@@ -1,6 +1,8 @@
 import logging
 import os
 import subprocess
+from tempfile import NamedTemporaryFile
+
 
 logger = logging.getLogger(__file__)
 
@@ -19,15 +21,24 @@ class Deb(object):
     @classmethod
     def create(cls, package_name, target_directory, files, options,
                dir_mappings):
-        deb = Deb(package_name, files, options, dir_mappings)
-        files = ["%s=%s" % (file, os.path.join(target_directory, file))
-                 for file in files]
-        files_str = ' '.join(files)
-        dir_mappings = ' '.join(dir_mappings)
-        cmd = 'fpm -t deb %s -n %s -s dir %s %s' % (
-            deb.options_str, package_name, dir_mappings, files_str
-        )
-        cmd_array = cmd.split(' ')
-        logger.debug("Running: %s", cmd)
-        subprocess.call(cmd_array)
-        return deb
+        # bzip compresses better, but takes a little longer
+        if not 'deb-compression' in options:
+            options['deb-compression'] = 'bzip2'
+
+        with NamedTemporaryFile(prefix='includes') as f:
+            deb = Deb(package_name, files, options, dir_mappings)
+            # these should be files only, fpm will create the directories for us if we need
+            # them. If you want an empty dir, has to be in dir_mappinmgs
+            files = ["%s=%s" % (file, os.path.join(target_directory, file))
+                     for file in files if os.path.isdir(file)]
+            files_str = '\n'.join(files)
+            dir_mappings = ' '.join(dir_mappings)
+            f.write(files_str)
+            f.flush()
+            cmd = 'fpm -t deb %s -n %s -s dir --inputs %s %s' % (
+                deb.options_str, package_name, '/home/shaw/include-files.txt', dir_mappings
+            )
+            cmd_array = cmd.split(' ')
+            logger.debug("Running: %s", cmd)
+            subprocess.call(cmd_array)
+            return deb

d4fe3f12ee653200e0b46fb2403b61884bc9790c
Shaw Vrana <shaw@vranix.com>
2018-05-25 16:35:07 -0700

    Dynamically load all target functions
    Just having fun...

--- fusilly/targets/__init__.py
+++ fusilly/targets/__init__.py
@@ -1,9 +1,24 @@
 from .targets import Target, Targets # noqa
 
-#
-# TODDO: these should all be added to the global namespace automatically
-#
-from .virtualenv import virtualenv_target   # noqa
-from .artifact import artifact_target   # noqa
-from .command import command_target     # noqa
-from .phony import phony_target     # noqa
+import os
+from importlib import import_module
+import sys
+
+cdir = os.path.abspath(os.path.dirname(__file__))
+
+IGNORE_LIST = [
+    '__init__.py',
+    'targets.py',
+]
+
+modules = [
+    f.split('.')[0] for f in os.listdir(cdir)
+    if os.path.isfile(os.path.join(cdir, f)) and
+    f not in IGNORE_LIST and f.endswith('.py')
+]
+
+for module_name in modules:
+    func_name = "%s_target" % module_name
+    module = import_module('.%s' % module_name, 'fusilly.targets')
+    target_func = getattr(module, func_name)
+    setattr(sys.modules['fusilly.targets'], func_name, target_func)
